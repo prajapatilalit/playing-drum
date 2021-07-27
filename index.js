b2316341dc929d79f0ced20ba5940f6e0bba044f
prajapatilalit <prajapatilalit150992@gmail.com>
2021-07-27 17:02:43 +0530

    set background image of drum element

---
+++ index.js
@@ -0,0 +1,10 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButton; i++) {
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function () {});
+}
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+// audio.play();
